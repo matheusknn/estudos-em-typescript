45a757a95a3f1ee560fdb1a1d64b9ef4bbff9ebe
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-04-13 20:21:37 -0300

    agrupando transações por data

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,7 +1,8 @@
 import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
+import { GrupoTransacao } from "./GrupoTransacao.js";
 
-let saldo: number = 3000;
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
 const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
   if (key == "data") {
     return new Date(value);
@@ -17,6 +18,7 @@ function debitar(valor: number): void {
     throw new Error("Saldo insuficiente");
   }
   saldo -= valor;
+  localStorage.setItem("saldo", saldo.toString());
 }
 
 function depositar(valor: number): void {
@@ -24,6 +26,7 @@ function depositar(valor: number): void {
     throw new Error("O valor a ser depositado deve ser maior que zero!")
   }
   saldo += valor;
+  localStorage.setItem("saldo", saldo.toString());
 }
 const Conta = {
   getSaldo() {
@@ -34,6 +37,25 @@ const Conta = {
     return new Date();
   },
 
+  getGruposTransacoes(): GrupoTransacao[] {
+    const gruposTransacoes: GrupoTransacao[] = [];
+    const listaTransacoes: Transacao[] = structuredClone(transacoes);
+    const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+    let labelAtualGrupoTransacao: string = "";
+
+    for (let transacao of transacoesOrdenadas) {
+      let labelGrupoTransacao: string = transacao.data.toLocaleDateString("pt-br", { month: "long", year: "2-digit" });
+      if (labelAtualGrupoTransacao != labelGrupoTransacao) {
+        labelAtualGrupoTransacao = labelGrupoTransacao;
+        gruposTransacoes.push({
+          label: labelGrupoTransacao,
+          transacoes: []
+        });
+      }
+      gruposTransacoes.at(-1).transacoes.push(transacao);
+    }
+    return gruposTransacoes;
+  },
   registrarTransacao(novaTransacao: Transacao): void {
     if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
       depositar(novaTransacao.valor);
@@ -46,7 +68,7 @@ const Conta = {
       throw new Error("tipo de transação é invalido!")
     }
     transacoes.push(novaTransacao);
-    console.log(novaTransacao);
+    console.log(this.getGruposTransacoes());
     localStorage.setItem("transacoes", JSON.stringify(transacoes));
   }
 }
